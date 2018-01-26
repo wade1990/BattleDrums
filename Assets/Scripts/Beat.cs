3d66da41208facd8897ff6cd38bd8fe8612ac5f0
Sam <sam_mie_sam@hotmail.com>
2018-01-27 00:49:16 +0100

    I'm an idiot.

---
+++ Assets/Scripts/Beat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum BeatType
+{
+    High,
+    Mid,
+    Low
+};
+
+public struct Beat
+{
+    public int Time;
+    BeatType Type;
+        
+    public Beat(int time, BeatType type)
+    {
+            
+        Time = time;
+        Type = type;
+    }
+
+        
+}
---
+++ Assets/Scripts/Rhythm.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Rhythm
+{
+    /// <summary>
+    /// List of Beats.
+    /// </summary>
+    private List<Beat> _sequence;   
+  
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sequence">The sequence </param>
+    public Rhythm(List<Beat> sequence)
+    {
+        _sequence = sequence;
+    }
+
+    /// <summary>
+    /// List of intervals, alternating on and off.
+    /// </summary>
+    public List<Beat> Sequence
+    {
+        get { return _sequence; }
+    }
+    
+    public void Append(Beat beat)
+    {
+        if (_sequence == null)
+            _sequence = new List<Beat>();
+        Sequence.Add(beat);
+    }
+
+    /// <summary>
+    /// Compares two sequences
+    /// </summary>
+    /// <returns>The error</returns>
+    public float Compare(Rhythm other)
+    {
+        float similarity = 0;
+
+
+        return 0;
+    }
+
+    /* TODO
+    public static Rhythm operator *(Rhythm rhythm, float speed)
+    {
+
+    }
+    */
+}
