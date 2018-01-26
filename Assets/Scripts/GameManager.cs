c2d635f4d307a28df89376080b16005ad7982af7
Jordy <jordy_van_opstal@live.nl>
2018-01-26 22:52:57 +0100

    Initial game manager mockup.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _linePoints;
+
+    [SerializeField] private PlayerPrefs _player1;
+    [SerializeField] private PlayerPrefs _player2;
+
+    private bool _finished;
+
+    private void Awake()
+    {
+        StartCoroutine(Loop());
+    }
+
+    private IEnumerator Loop()
+    {
+        yield return InitialCountDown();
+
+        while (!_finished)
+        {
+            yield return InputPhase();
+            yield return ResolveActions();
+        }
+    }
+
+
+    private object InitialCountDown()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    private object InputPhase()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private object ResolveActions()
+    {
+        throw new System.NotImplementedException();
+    }
+}
