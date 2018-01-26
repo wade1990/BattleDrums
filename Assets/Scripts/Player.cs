ce8d1fbc30727bb6d994519c3f27d7c5242d4387
Jordy <jordy_van_opstal@live.nl>
2018-01-26 22:57:17 +0100

    Add empty methods to player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Assets.Scripts
+﻿namespace Assets.Scripts
 {
-    class Player
+    internal class Player
     {
+        public void StartReadingInput()
+        {
+            
+        }
+
+        public void StopReadingInput()
+        {
+            
+        }
+
+
     }
 }
