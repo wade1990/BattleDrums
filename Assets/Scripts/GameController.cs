6895d969716b264dd269b20c15ba9755925a1a65
Jordy <jordy_van_opstal@live.nl>
2018-01-27 00:40:49 +0100

    Remove unnessary field.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,9 +11,7 @@ public class GameController : MonoBehaviour
 
     [SerializeField] private Player _leftPlayer;
     [SerializeField] private Player _rightPlayer;
-
-    private bool _finished;
-
+    
     private int _activeLineIndex;
 
     /// <summary>
