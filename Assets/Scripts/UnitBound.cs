60157be783708bd02ac54e994fc1666a38671b22
Jordy <jordy_van_opstal@live.nl>
2018-01-28 00:57:03 +0100

    Add bounds so the units don't walk out of the field.

---
+++ Assets/Scripts/UnitBound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class UnitBound : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Unit unit = collision.transform.GetComponent<Unit>();
+        if (unit != null)
+            unit.StopMoving();
+    }
+}
