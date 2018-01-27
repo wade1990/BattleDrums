4729bedff7ecb60d439b5b4222823da82af5836e
kamipenguin <darkroxy@live.nl>
2018-01-27 12:11:01 +0100

    Added move functionality

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -3,10 +3,19 @@
 public class MoveController : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Vector3 moveDirection;
+
+    [SerializeField] private bool isMoving;
+
+    private void Update()
+    {
+        if (isMoving)
+            StartMoving();
+    }
 
     private void StartMoving()
     {
-       
+        gameObject.transform.position += moveDirection;
     }
 
     private void StopMoving()
