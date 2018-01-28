36d941570d462fee985c788edcc8fc76207e6217
Jordy <jordy_van_opstal@live.nl>
2018-01-28 14:05:46 +0100

    Add drummer.

---
+++ Assets/Scripts/Units/DrumController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DrumController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Animator animator = GetComponent<Animator>();
+        InputHandler inputHandler = GetComponentInParent<InputHandler>();
+
+        inputHandler.BeatMade += beat => animator.SetTrigger("Beat");
+    }
+}
