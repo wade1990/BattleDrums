2d0500b674381aef5e1aa068e20aaae6944e55b7
kamipenguin <darkroxy@live.nl>
2018-01-28 11:33:51 +0100

    Added GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public AudioClip GameMusic;
+    public AudioClip EndMusic;
+
     private AudioSource _audioSource;
+    private float _musicLength;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
 	// Use this for initialization
 	void Start ()
